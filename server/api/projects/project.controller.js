5e6f241357566fd3d55ac349d72a5bef83d6efcd
Saulo Aguiar <sauloaguiar89@gmail.com>
2025-01-03 12:26:37 -0300

    adds check for existing user and role in order to create a project

--- server/api/projects/project.controller.js
+++ server/api/projects/project.controller.js
@@ -3,14 +3,18 @@ const projectService = require('./project.service');
 exports.createProject = async (req, res) => {
   try {
     const { name, description } = req.body;
-
+    const userId = req.session.userId;
+    if (!userId) {
+      return res.status(401).json({ error: 'Unauthorized: No user logged in' });
+    }
     if (!name || !description) {
       return res.status(400).json({ error: 'Name and description are required' });
     }
 
-    const project = await projectService.createProject({ name, description });
+    const project = await projectService.createProject({ userId, name, description });
     res.status(201).json(project);
   } catch (error) {
+    console.log(error);
     res.status(500).json({ error: error.message });
   }
 };
--- server/api/projects/project.service.js
+++ server/api/projects/project.service.js
@@ -1,10 +1,24 @@
 const knex = require("../../database/connection.js");
 
-exports.createProject = async ({ name, description }) => {
-  const [project] = await knex('projects')
-    .insert({ name, description })
-    .returning(['id', 'name', 'description', 'deleted', 'created_at', 'updated_at']);
-  return project;
+exports.createProject = async ({ name, description, userId }) => {
+  return knex.transaction(async trx => {
+    try {
+      const [project] = await trx('projects')
+        .insert({ name, description })
+        .returning(['id', 'name', 'description', 'deleted', 'created_at', 'updated_at']);
+      
+      const user = await trx('users').where({ id: userId }).first();
+      
+      await trx('user_projects')
+        .insert({ user_id: userId, project_id: project.id, role_id: user.role_id });
+      
+      await trx.commit();
+      return project;
+    } catch (error) {
+      await trx.rollback();
+      throw error;
+    }
+  });
 };
 
 exports.deleteProject = async (id) => {
